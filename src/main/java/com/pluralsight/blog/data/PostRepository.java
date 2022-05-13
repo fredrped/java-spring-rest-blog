6360ce6cd95f86c7a2dd58b4877e4dd652582dc8
Fredrik Pedersen <fredrik.pedersen@tryg.no>
2022-05-13 14:20:53 +0200

    missing s

--- src/main/java/com/pluralsight/blog/data/PostRepository.java
+++ src/main/java/com/pluralsight/blog/data/PostRepository.java
@@ -7,7 +7,7 @@ import org.springframework.data.rest.core.annotation.RestResource;
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Long> {
-    @RestResource(rel = "contain-title", path = "containsTitle")
+    @RestResource(rel = "contains-title", path = "containsTitle")
     List<Post> findByTitleContaining(String title);
 };
 
